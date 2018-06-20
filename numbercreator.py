51691f559a82c70b96e45c5176c7adc2fd0592ca
Vítor Barcellos <vtor.barcellos7@gmail.com>
2018-06-20 09:46:06 -0300

    Melhorando a nomenclatura das funções e variáveis

--- numbercreator.py
+++ numbercreator.py
@@ -1,32 +1,41 @@
 import random as rd
 import numpy.random
 
-def calc_lambda(ro, EX):
+# Recebe ro e E[X]
+# Retorna lambda tal que lambda = ro / E[X]
+def calcula_lambda(ro, EX):
     return ro/EX
 
-def generete_package_size_dados():
-    tam_pacote=numpy.random.choice(numpy.arange(1, 5), p=[0.3, 0.1, 0.3, 0.3])
-    print tam_pacote
-    if tam_pacote==4:
-        true_tam=numpy.random.randint(64,1500)
-    elif tam_pacote==1:
-        true_tam=64
-    elif tam_pacote==2:
-        true_tam=512
+# Retorna uma amostra aleatoria do tamanho em bytes de um pacote de dados
+# (Amostra da variavel aleatoria X1 - ver descricao do trabalho)
+def gera_tamanho_pacote_dados():
+    # 0.3 de chance de indice=1, 0.1 de chance de indice=2, e assim por diante
+    indice=numpy.random.choice(numpy.arange(1, 5), p=[0.3, 0.1, 0.3, 0.3])
+    print indice
+    if indice==4:
+        tamanho=numpy.random.randint(64,1500)
+    elif indice==1:
+        tamanho=64
+    elif indice==2:
+        tamanho=512
     else:
-        true_tam=1500
-    return true_tam
+        tamanho=1500
+    return tamanho
 
-def chegada_pacote_voz(intervalo_deterministico=0.016, tamanho_pacote=512):
+# def chegada_pacote_voz(intervalo_deterministico=0.016, tamanho_pacote=512):
 
-def numero_pacotes_atividade_voz():
+# Retorna uma amostra aleatoria do numero de pacotes de voz durante um periodo de atividade
+# (Amostra de uma distribuicao geometrica com media de 22 pacotes)
+def gera_numero_pacotes_voz():
     return numpy.random.geometric(float(1)/22)
 
-def periodo_atividade_voz(n_pacotes):
+# Recebe numero de pacotes de voz de um periodo de atividade
+# Retorna duracao em milisegundos do periodo de atividade
+def calcula_duracao_periodo_atividade_voz(n_pacotes):
     return 16*n_pacotes
 
 
-true_tam = generete_package_size_dados()
+true_tam = gera_tamanho_pacote_dados()
 
 X1 = (float(true_tam)*8)/2097152
 
