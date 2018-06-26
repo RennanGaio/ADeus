8ac59dbf1e9a8e12448010289ae20144c20024c6
Vítor Barcellos <vtor.barcellos7@gmail.com>
2018-06-26 20:18:08 -0300

    Classe pacote de dados implementada

--- numbercreator.py
+++ numbercreator.py
@@ -5,11 +5,25 @@ import numpy.random
 # Declaracao de variaveis globais
 # **************************************************************************
 
+modo_debug = False
+
+# Recebe ro e E[X]
+# Retorna lambda tal que lambda = ro / E[X]
+def calcula_lambda(ro, EX):
+    return ro/EX
+
 # E[L] onde L (variavel aleatoria) eh o numero de bytes de um pacote de dados
-EL = 735.8
+EL = float(755)
 
 # E[X1] onde X1 (variavel aleatoria) eh o tempo de servico de um pacote de dados
-EX1 = (EL*8)/2097152
+EX1 = (EL*8)/float(2097152)
+
+# ro1, utilizacao do sistema levando em conta apenas os pacotes de dados
+# ro1 = lambda1 * E[X1] -- ver descricao do trabalho
+ro1 = 0.1
+
+# lambda1, taxa de chegada dos pacotes de dados
+lamb1 = calcula_lambda(ro1, EX1)
 
 # Tamanho fixo dos pacotes de voz, em bits
 tamanho_pacote_voz = 512
@@ -21,10 +35,7 @@ tempo_servico_pacote_voz = tamanho_pacote_voz/2097152
 media_periodo_silencio = 0.650
 
 
-# Recebe ro e E[X]
-# Retorna lambda tal que lambda = ro / E[X]
-def calcula_lambda(ro, EX):
-    return ro/EX
+
 
 # **************************************************************************
 # Funcoes dos pacotes de dados
@@ -33,10 +44,10 @@ def calcula_lambda(ro, EX):
 # Retorna uma amostra aleatoria do tamanho em bytes de um pacote de dados
 # (Amostra da variavel aleatoria L - ver descricao do trabalho)
 def gera_tamanho_pacote_dados():
-    print "--> Entrou em [gera_tamanho_pacote_dados]"
+    if modo_debug: print "--> Entrou em [gera_tamanho_pacote_dados]"
     # 0.3 de chance de indice=1, 0.1 de chance de indice=2, e assim por diante
     indice=numpy.random.choice(numpy.arange(1, 5), p=[0.3, 0.1, 0.3, 0.3])
-    print "\tindice: [" + str(indice) + "]"
+    if modo_debug: print "\tindice: [" + str(indice) + "]"
     if indice==4:
         tamanho=numpy.random.randint(64,1500)
     elif indice==1:
@@ -45,28 +56,28 @@ def gera_tamanho_pacote_dados():
         tamanho=512
     else:
         tamanho=1500
-    print "\ttamanho: [" + str(tamanho) + "]"
-    print "--> Saiu de [gera_tamanho_pacote_dados]"
+    if modo_debug: print "\ttamanho: [" + str(tamanho) + "]"
+    if modo_debug: print "--> Saiu de [gera_tamanho_pacote_dados]"
     return tamanho
 
 # Retorna uma amostra aleatoria do tempo de servico em segundos de um pacote de dados
 # (Amostra da variavel aleatoria X1 - ver descricao do trabalho)
 def gera_tempo_servico_pacote_dados():
-    print "--> Entrou em [gera_tempo_servico_pacote_dados]"
+    if modo_debug: print "--> Entrou em [gera_tempo_servico_pacote_dados]"
     tamanho = gera_tamanho_pacote_dados()
     # Converte o tamanho de bytes para bits (multiplicando por 8), e divide por 2Mbps para obter o tempo de transmissao
     tempo_servico = float(tamanho*8)/2097152
-    print "\ttempo_servico: [" + str(tempo_servico) + "]"
-    print "--> Saiu de [gera_tempo_servico_pacote_dados]"
+    if modo_debug: print "\ttempo_servico: [" + str(tempo_servico) + "]"
+    if modo_debug: print "--> Saiu de [gera_tempo_servico_pacote_dados]"
     return tempo_servico
 
 # Retorna uma amostra aleatoria de uma chegada de um pacote de dados em segundos
 # (Amostra de uma variavel exponencial com taxa lamb = ro/E[X1])
 def gera_chegada_pacote_dados(lamb):
-    print "--> Entrou em [gera_chegada_pacote_dados]"
+    if modo_debug: print "--> Entrou em [gera_chegada_pacote_dados]"
     tempo_chegada = numpy.random.exponential(1/lamb)
-    print "\ttempo_chegada: [" + str(tempo_chegada) + "]"
-    print "--> Saiu de [gera_chegada_pacote_dados]"
+    if modo_debug: print "\ttempo_chegada: [" + str(tempo_chegada) + "]"
+    if modo_debug: print "--> Saiu de [gera_chegada_pacote_dados]"
     return tempo_chegada
 
 # **************************************************************************
@@ -78,42 +89,57 @@ def gera_chegada_pacote_dados(lamb):
 # Retorna uma amostra aleatoria do numero de pacotes de voz durante um periodo de atividade
 # (Amostra de uma distribuicao geometrica com media de 22 pacotes)
 def gera_numero_pacotes_voz():
-    print "--> Entrou em [gera_numero_pacotes_voz]"
+    if modo_debug: print "--> Entrou em [gera_numero_pacotes_voz]"
     numero_pacotes = numpy.random.geometric(float(1)/22)
-    print "\tnumero_pacotes: [" + str(numero_pacotes) + "]"
-    print "--> Saiu de [gera_numero_pacotes_voz]"
+    if modo_debug: print "\tnumero_pacotes: [" + str(numero_pacotes) + "]"
+    if modo_debug: print "--> Saiu de [gera_numero_pacotes_voz]"
     return numero_pacotes
 
 # Recebe numero de pacotes de voz de um periodo de atividade
 # Retorna duracao em segundos do periodo de atividade
 def calcula_duracao_periodo_atividade_voz(n_pacotes):
-    print "--> Entrou em [calcula_duracao_periodo_atividade_voz]"
+    if modo_debug: print "--> Entrou em [calcula_duracao_periodo_atividade_voz]"
     duracao = (0.016)*n_pacotes
-    print "\tduracao: [" + str(duracao) + "]"
-    print "--> Saiu de [calcula_duracao_periodo_atividade_voz]"
+    if modo_debug: print "\tduracao: [" + str(duracao) + "]"
+    if modo_debug: print "--> Saiu de [calcula_duracao_periodo_atividade_voz]"
     return duracao
 
 # Retorna uma amostra aleatoria da duracao do periodo de silencio dos pacotes de voz
 # (Amostra de uma distribuicao exponencial com media de 650ms)
 def calcula_duracao_periodo_silencio_voz():
-    print "--> Entrou em [calcula_duracao_periodo_silencio_voz]"
+    if modo_debug: print "--> Entrou em [calcula_duracao_periodo_silencio_voz]"
     duracao = numpy.random.exponential(media_periodo_silencio)
-    print "\tduracao: [" + str(duracao) + "]"
-    print "--> Saiu de [calcula_duracao_periodo_silencio_voz]"
+    if modo_debug: print "\tduracao: [" + str(duracao) + "]"
+    if modo_debug: print "--> Saiu de [calcula_duracao_periodo_silencio_voz]"
     return duracao
 
+# **************************************************************************
+# Classes
+# **************************************************************************
+
+class pacote_dados:
+
+    def __init__(self):
+        self.tempo_chegada = gera_chegada_pacote_dados(lamb1)
+        self.tamanho  = gera_tamanho_pacote_dados()
+        self.tempo_servico = gera_tempo_servico_pacote_dados()
+
+    
 
 # **************************************************************************
 # Programa principal
 # **************************************************************************
 
-print "--> Iniciou programa principal"
-ro = 0.1
-tempo_chegada_dados = gera_chegada_pacote_dados(calcula_lambda(ro, EX1))
-print tempo_chegada_dados
-X1 = gera_tempo_servico_pacote_dados()
-print X1
-amostra_periodo_silencio = calcula_duracao_periodo_silencio_voz()
-print amostra_periodo_silencio
-amostra_periodo_atividade = calcula_duracao_periodo_atividade_voz(gera_numero_pacotes_voz())
-print "--> Terminou programa principal"
+if modo_debug: print "--> Iniciou programa principal"
+
+#tempo_chegada_dados = gera_chegada_pacote_dados(lamb)
+#print tempo_chegada_dados
+#X1 = gera_tempo_servico_pacote_dados()
+#print X1
+#amostra_periodo_silencio = calcula_duracao_periodo_silencio_voz()
+#print amostra_periodo_silencio
+#amostra_periodo_atividade = calcula_duracao_periodo_atividade_voz(gera_numero_pacotes_voz())
+
+for i in range(0, 20): print pacote_dados().tempo_chegada
+
+if modo_debug: print "--> Terminou programa principal"
